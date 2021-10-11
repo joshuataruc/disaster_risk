0314b0fde92c0d2c4f28801706f2cb8677fe59d4
JOSHUA TARUC <imtaruc25@gmail.com>
2021-10-11 15:59:00 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>DPRRP</title>
+</head>
+<body>
+    <?php include_once 'nav.php'; ?>
+    <div>Main</div>
+    <div>news</div>
+    <div>what to do</div>
+    <div>contacts</div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+   <nav>
+    <input type="checkbox" name="" id="check">
+    <label for="check" class="checkbtn">
+     <i class="fas fa-bars"></i>
+   </label>
+   <label class="nav-logo">DPRRP </label>
+   <ul>
+     <li><a href="#home">Home</a></li>
+     <li><a href="#contact">What to do</a></li>
+     <li><a href="#about">Contacts</a></li>
+     <li><a href="login.php">Register</a></li>
+     <li><a href="login.php">Login</a></li>
+   </ul>
+ </nav> 
+</body>
+</html>
