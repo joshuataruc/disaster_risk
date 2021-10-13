b8a514e427f50f7f33021c85e28a28a971298c30
Joshua Taruc <imtaruc25@gmail.com>
2021-10-13 21:09:30 +0800

    added card

--- index.php
+++ index.php
@@ -14,11 +14,16 @@
         <div class="main-text">
             <h1>Disaster Preparedness and Risk Reduction Portal</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sit ipsam voluptatem, odio quo dolorem similique quod? Repudiandae inventore tempore ab consectetur nihil nam, dolor tenetur et! Quam, blanditiis officiis?</p>
-            <a href="#"class="btn-action">Call to Action</a> 
+            <a href="#"class="btn-action">Call to Action</a>
         </div>
     </section>
-    <div class="news">news</div>
-    <div>what to do</div>
+    <section class="what-to-do" >what to do
+        <div class="card">
+            <img src="image/fire.jpg" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
+            <a href="#"class="card-btn">Watch Video</a>
+        </div>
+    </section>
     <div>contacts</div>
 </body>
 </html>
