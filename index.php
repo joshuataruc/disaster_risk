a226a1d0c9656cd455cb79ee58055d386fe7e720
Joshua Taruc <imtaruc25@gmail.com>
2021-10-15 16:55:27 +0800

    finished cards

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>DPRRP</title>
 </head>
 <body>
@@ -17,12 +18,51 @@
             <a href="#"class="btn-action">Call to Action</a>
         </div>
     </section>
-    <section class="what-to-do" >what to do
-        <div class="card">
-            <img src="image/fire.jpg" alt="">
+
+   
+    <section class="what-to-do" >
+    <h1 class="what-to-do-title">WHAT TO DO WHEN</h1>
+        <div class="what-to-do-content">
+            <div class="card">
+            <img src="image/flood.jpg" alt="">
+            <div class="card-body">
+                <h3>Title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
-            <a href="#"class="card-btn">Watch Video</a>
+            </div>
+            <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
         </div>
+        <div class="card">
+            <img src="image/flood.jpg" alt="">
+            <div class="card-body">
+                <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
+            </div>
+            <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
+        </div>
+        <div class="card">
+            <img src="image/flood.jpg" alt="">
+            <div class="card-body">
+                <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
+            </div>
+            <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
+        </div>
+        <div class="card">
+            <img src="image/flood.jpg" alt="">
+            <div class="card-body">
+                <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
+            </div>
+            <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
+        </div>
+        <div class="card">
+            <img src="image/flood.jpg" alt="">
+            <div class="card-body">
+                <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo nihil unde delectus autem reiciendis animi doloribus deleniti facere, ut ratione accusantium sint architecto incidunt facilis. Quod officia ipsa enim!</p>
+            </div>
+            <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
+        </div></div>
     </section>
     <div>contacts</div>
 </body>
