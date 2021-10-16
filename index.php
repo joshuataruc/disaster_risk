aa958bbdc5974abbd148d5cfa42e88ba9c0753c3
Joshua Taruc <imtaruc25@gmail.com>
2021-10-16 23:13:25 +0800

    added table for the contact list of hospitals

--- index.php
+++ index.php
@@ -64,8 +64,58 @@
             <a href="#"class="card-btn">Play Video <span class="btn-icon"><i class="fas fa-play-circle"></i> </span> </a>
         </div></div>
     </section>
-    <div class="contact">
-        
+    <div class="contacts">
+        <h1>LIST OF HOSPITALS IN TARLAC</h1>
+        <div class="table-container">
+            <table class="contacts-table">
+            <thead>
+                <tr>
+                    <th>Hospital</th>
+                    <th>Contact Number</th>
+                    <th>Address</th>
+                    <th>Map</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Tarlac Provincial Hospital</td>
+                    <td>(045) 982-6885 <br>(045) 491-8970</td>
+                    <td>Hospital Drive San Vicente, Tarlac City, Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Tarlac+Provincial+Hospital+(TPH)/@15.4750177,120.5865725,15z/data=!4m12!1m6!3m5!1s0x0:0xcaf4807a9e1a2384!2sTarlac+Provincial+Hospital+(TPH)!8m2!3d15.4750177!4d120.5865725!3m4!1s0x3396c7fc3a972c49:0xcaf4807a9e1a2384!8m2!3d15.4750177!4d120.5865725"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Central Luzon Doctors' Hospital</td>
+                    <td>(045) 982-0806 <br>(045) 982-0661 <br>(045) 982-8678</td>
+                    <td>Hospital Drive San Vicente, Tarlac City, Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Central+Luzon+Doctors'+Hospital/@15.4782732,120.5861805,15.17z/data=!4m5!3m4!1s0x3396c6464a90eb69:0x9c71e958e91bdd30!8m2!3d15.4781792!4d120.5869477"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Jecsons Medical Center</td>
+                    <td>(045) 982-5501 <br>(045) 982-5504 <br>(045) 982-1457 <br> (045) 982-5507</td>
+                    <td>MacArthur High way, Tarlac City, Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Jecsons+Medical+Center/@15.4802873,120.5816975,15z/data=!4m5!3m4!1s0x3396c618732e9559:0x3dd80301565b3a7!8m2!3d15.4909959!4d120.5974886"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Ramos General Hospital</td>
+                    <td>(045) 982-0542 <br>(045) 982-2350 <br>(045) 982-7074 <br> (045) 982-4041 <br> (045) 982-8725 </td>
+                    <td>Hilario St, Tarlac City, Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Ramos+General+Hospital/@15.4778386,120.5855109,16z/data=!4m5!3m4!1s0x3396c63e9c2d4071:0x518aeb1a9159c146!8m2!3d15.484983!4d120.5931144"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Talon General Hospital</td>
+                    <td>(045) 982-1400</td>
+                    <td>F. Tañedo St, Tarlac City, 2300 Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Talon+General+Hospital/@15.4906328,120.5906914,18.17z/data=!4m5!3m4!1s0x3396c623549bb29f:0x8633a42602e8ecf9!8m2!3d15.4907929!4d120.5915393"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+                <tr>
+                    <td>Loving Mother</td>
+                    <td>(045) 982 5644</td>
+                    <td>F. Tañedo St, Tarlac City, 2300 Tarlac</td>
+                    <td><a href="https://www.google.com/maps/place/Loving+Mother+General+hospital+%26+Diagnostic+Center/@15.4927742,120.5931056,18.17z/data=!4m5!3m4!1s0x3396c62340414249:0x5245366b15dc8ec4!8m2!3d15.4928178!4d120.5933487"><i class="fas fa-map-marked"></i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
     </div>
 </body>
 </html>
